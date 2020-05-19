189a4598062f8b23f763b431c995ad59d8602b06
Philip Helgesen <55789033+PhilipHelgesen@users.noreply.github.com>
2020-05-19 12:25:46 +0200

    separerte kommentarer i html og js

---
+++ Kommentarer.js
@@ -0,0 +1,44 @@
+
+function renderMessageList() {
+    //Returns the enterd data in LocalStorage as a String
+    const messageList = JSON.parse(window.localStorage.getItem("messageList")) || [];
+    // parses the String into an object
+    const messageListEl = document.getElementById("messageList");
+
+    messageListEl.innerHTML = "";
+    for (const message of messageList) {
+        const messageEl = document.createElement("div");
+        const {
+            name,
+            comment
+        } = message;
+        messageEl.innerHTML = `<h4>${name}</h4>
+                              <div>${comment}</div>`;
+        messageListEl.appendChild(messageEl);
+    }
+}
+
+function createNewComment(event) {
+
+    // HINDRER AT URL OPPDATERES
+    event.preventDefault();
+
+    const name = document.querySelector("[name='name']").value;
+    const comment = document.querySelector("[name='content']").value;
+
+    // EN SNARVEI FOR Å SKRIVE {NAME: NAME, COMMENT: COMMENT}
+    const message = {
+        name,
+        comment
+    };
+
+    const messageList = JSON.parse(window.localStorage.getItem("messageList")) || [];
+    messageList.push(message);
+    // LAGRER DATA I STORAGE TIL BROWSER
+    window.localStorage.setItem("messageList", JSON.stringify(messageList));
+
+    
+    event.target.reset();
+}
+
+
