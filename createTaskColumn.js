154ece443d857ca16e6cffcc81380caf6f993a09
Lasse Wulff <lasseheggemwulff@iti-iti.local>
2020-05-20 11:56:14 +0200

    legge til oppgave kolonne test

---
+++ createTaskColumn.js
@@ -0,0 +1,26 @@
+function newTaskColumn() {
+   
+    let standardNrOfColumns = 3;
+    
+    let actionArray = ["To Do", "Doing", "Done"];
+    let colorStyleArray = ["green", "blue", "yellow"];
+    
+    for(i = 0; i < standardNrOfColumns; i++){
+    //generere bokser
+    let column = document.createElement("div"); column.setAttribute("id", "column" + i);
+    column.style.height = "200px";
+    column.style.width = "100px";
+    column.style.backgroundColor = colorStyleArray[i];
+        
+    column.innerHTML = "<h4>" + actionArray[i] + "</h4>";
+    
+    let currentDiv = document.getElementById("TaskContainer");
+    
+    
+    
+    column.appendChild(currentDiv);
+    
+    }
+    
+    
+}
