fd4282902233aeb188a60a9cccd5c1ebee5ab75f
Even Matre Larsen <even@Evens-MacBook-Pro.local>
2020-05-21 12:47:05 +0200

    added functionallity to addMembers.js and connected to index.html

--- addMembers.js
+++ addMembers.js
@@ -1,39 +1,46 @@
-function addNewMember(Event) {
-    var member = prompt("Please enter team member:");
+function addNewMember(event) {
+    
+    event.preventDefault();
+    
+    const member = document.querySelector("[name = 'memberName']").value;
     
     const nameList = JSON.parse(window.localStorage.getItem("nameList")) || [];
     nameList.push(member);
     window.localStorage.setItem("nameList", JSON.stringify(nameList));
     
+    
+    event.target.reset();
+    
     memberElement();
     
+    
 }
-function addMemberDiv(Event) {
-
-    var ok = true;
-
-     if (ok === true) {
-          var div = document.createElement('div');
-
-          div.className = 'new-member';
-            //div.style.backgroundColor = "black";
-
-
-         document.getElementById('MemberContainer')[0].appendChild(div);
-     }
-}
-
-memberElement();
 
 function memberElement(){
     
     const nameList = JSON.parse(window.localStorage.getItem("nameList")) || [];
-    const listOfMembersEl = document.getElementById("TeamMember");
-    var counter = 0;
-    for (member of nameList){
+    
+    let memberDiv = document.getElementById("MemberContainer");
+    
+    memberDiv.innerHTML= "";
+    
+    for (const member of nameList){
+        const memberListEl = document.createElement("div");
         const memberEl = document.createElement("div");
-        memberEl.innerHTML = `<li>${nameList[counter]}</li>`;
-        listOfMembersEl.appendChild(memberEl);
+        
+        
+        memberEl.innerHTML = `
+            <h4>${member}</h4>`;
+        
+        memberListEl.style.width = "110px";
+        memberListEl.style.height = "110px";
+        memberListEl.style.display = "block";
+        memberListEl.style.border = "1px solid black";
+        memberListEl.style.borderRadius = "50%";
+        memberListEl.style.textAlign = "center";
+        
+        memberListEl.appendChild(memberEl);
+        
+        memberDiv.appendChild(memberListEl);
     }
 }
-
