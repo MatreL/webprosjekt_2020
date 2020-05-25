d8a29d74fe2b89c6c57413007d351e7c22b77d43
Pål-Øivind Mentveit <gamingtruble.3@gmail.com>
2020-05-25 11:48:32 +0200

    some minor size adjustments to collumns, and grid system for page2

--- createTaskColumn.js
+++ createTaskColumn.js
@@ -53,7 +53,8 @@ function renderColumns(){
         //Styling av bokser
         columnEl.style.gridColumn = `${i + 1}/${i + 2}`;
         columnEl.style.gridRow = "2/3";
-        columnEl.style.overflowY= "scroll";
+        columnEl.style.overflowY = "scroll";
+        columnEl.style.margin = "1em";
         columnEl.style.backgroundColor = columnList[i].color;
         //genererer headertext
         columnHead.innerHTML = `<h4 class="headerText"> ${columnList[i].title} </h4>`;
