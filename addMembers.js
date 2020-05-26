f8a322b98c4da7955d7672a182f43f2789598df7
Pål-Øivind Mentveit <gamingtruble.3@gmail.com>
2020-05-26 20:46:35 +0200

    changed background and textcolors for member elements

--- addMembers.js
+++ addMembers.js
@@ -2,35 +2,35 @@ memberElement();
 
 //Funksjon for å legge verdier inn i localstorage
 function addNewMember(event) {
-    
+
     event.preventDefault();
-    
+
     const memberName = document.querySelector("[name = 'memberName']").value;
-    
+
     const member = {memberName};
-    
+
     const nameList = JSON.parse(window.localStorage.getItem("nameList")) || [];
     nameList.push(member);
     window.localStorage.setItem("nameList", JSON.stringify(nameList));
-    
+
     event.target.reset();
-    
+
     memberElement();
 }
 
 //funksjon som skal hente verdier i localstorage samtidig som den oppretter elementet verdiene skal ligge i
 function memberElement(){
-    
+
     const nameList = JSON.parse(window.localStorage.getItem("nameList")) || [];
-    
+
     let memberDiv = document.getElementById("newMemberDiv");
     memberDiv.innerHTML = "";
-    
+
     let memberSelectEl = document.getElementById("memberSelect");
     memberSelectEl.innerHTML ="";
 
     for (const member of nameList){
-        
+
         const memberListEl = document.createElement("div");
         const memberEl = document.createElement("div");
         const {memberName} = member;
@@ -41,18 +41,20 @@ function memberElement(){
 
         memberEl.innerHTML = `
             <h4>${memberName}</h4>`;
-        
+
         memberListEl.style.width = "80px";
         memberListEl.style.height = "80px";
-        memberListEl.style.border = "1px solid black";
+        memberListEl.style.border = "1px solid #2D5F5D";
         memberListEl.style.borderRadius = "50%";
         memberListEl.style.textAlign = "center";
         memberListEl.style.float = "left";
         memberListEl.style.margin = "3px";
-        
+        memberListEl.style.backgroundColor = "#022140"
+        memberListEl.style.color = "#FFFFFF"
+
         memberListEl.draggable = true;
         memberListEl.ondragstart = event => handleDragMemberStart(event, memberName);
-        
+
         memberListEl.appendChild(memberEl);
         memberDiv.appendChild(memberListEl);
     }
